15e94c8c0c659e4da5134e71cb76308c69defd36
Seongho Jang <jangsung0212@naver.com>
2022-10-25 18:57:31 +0900

    10.25

--- pythonsource/NQueen.py
+++ pythonsource/NQueen.py
@@ -1,5 +1,10 @@
 global N
 N = 4
+import random
+n = int(input())
+POPULATION_SIZE = n			# 개체 집단의 크기
+MUTATION_RATE = 0.1			# 돌연 변이 확률
+SIZE = n				# 하나의 염색체에서 유전자 개수		
  
 def printSolution(board):
     for i in range(N):
@@ -7,13 +12,6 @@ def printSolution(board):
             print (board[i][j],end=' ')
         print()
  
- 
-# A utility function to check if a queen can
-# be placed on board[row][col]. Note that this
-# function is called when "col" queens are
-# already placed in columns from 0 to col -1.
-# So we need to check only left side for
-# attacking queens
 def isSafe(board, row, col):
  
     # Check this row on left side
@@ -34,40 +32,20 @@ def isSafe(board, row, col):
     return True
  
 def solveNQUtil(board, col):
-    # base case: If all queens are placed
-    # then return true
     if col >= N:
         return True
- 
-    # Consider this column and try placing
-    # this queen in all rows one by one
     for i in range(N):
  
         if isSafe(board, i, col):
-            # Place this queen in board[i][col]
             board[i][col] = 1
  
-            # recur to place rest of the queens
             if solveNQUtil(board, col + 1) == True:
                 return True
  
-            # If placing queen in board[i][col
-            # doesn't lead to a solution, then
-            # queen from board[i][col]
             board[i][col] = 0
  
-    # if the queen can not be placed in any row in
-    # this column col  then return false
     return False
  
-# This function solves the N Queen problem using
-# Backtracking. It mainly uses solveNQUtil() to
-# solve the problem. It returns false if queens
-# cannot be placed, otherwise return true and
-# placement of queens in the form of 1s.
-# note that there may be more than one
-# solutions, this function prints one  of the
-# feasible solutions.
 def solveNQ():
     board = [ [0, 0, 0, 0],
               [0, 0, 0, 0],
